5cf4a21b39feddd57247b43d52cead68169af3bd
Eric Dodgion <Barry.manowar@gmail.com>
2021-04-01 13:01:19 -0600

    recent searches modal

--- assets/script.js
+++ assets/script.js
@@ -1,132 +1,158 @@
 var input = document.querySelector(".input");
 //needed to change class of button to an id because of the 'recent seach' button
 var button = document.querySelector("#searchButton");
+var input = document.querySelector(".input");
+var bioEl = document.querySelector(".bio");
+var genreEl = document.querySelector(".genre");
+var countryEl = document.querySelector(".country");
+var eventAmountEl = document.querySelector(".eventAmount");
+var eventUrlEl = document.querySelector(".eventUrl");
+var artistLogo = document.querySelector(".logo");
+var recentSearchArr = JSON.parse(localStorage.getItem('previousSearches')) || [];
 
 //Eric working on recent search modal loop and function
-    for (var i = 0; i < localStorage.length; i++) {
-        var recentSearchArr = JSON.parse(localStorage.getItem(i)) ;
-        var artistName = $(".list-group").addClass("list-group-item");
-        artistName.append("<li>" + recentSearchArr + "</li>");
-      }
-    
-var recentButton = document.getElementById("recent-search");
-var modal = document.getElementById("recent-search");
 
-recentButton.onclick = function() {
-    modal.style.display = 'block';
+var recentSearchModal = document.getElementById("search-modal");
+
+var recentButton = document.getElementById("recent-search").addEventListener("click", function(){
+    recentSearchModal.setAttribute("class", "modal is-active")
+})
+
+var exitSeachModal = document.querySelector(".modal-close").addEventListener("click", function () {
+    recentSearchModal.setAttribute("class", "modal hide");
+  });
+
+//recentButton.addEventListener("click", function(){
+  //recentSearchModal.querySelector("class", "modal is-active");
+//};
+
+function generateBtns() {
+  var buttonGroup = document.querySelector("#search-modal");
+ 
+  recentSearchArr.reverse().forEeach(function (artist) {
+    var artistBtn = document.createElement("<button>");
+    artistBtn.textContent = artist;
+    artistBtn.addEventListener("click", handleRecentSearchFormSubmit);
+    buttonGroup.append(artistBtn);
+  });
 }
 
-function recentSearch(event) {
-    event.preventDefault();
+/*function getParams(search) {
+  console.log(search);
 
-    var btnGroup = document.querySelector('#search-modal');
-    recentSearchArr.reverse().
+  if (recentSearchArr.length <= 10) {
+    recentSearchArr.push(search);
+    localStorage.setItem('previousSearches', JSON.stringify(recentSearchArr));
+  }
+  getArtistInfo(search);
+}*/
 
+function handleRecentSearchFormSubmit(event) {
+  event.preventDefault();
+  console.log("works");
 
-    
-    forEach(function(search){
-        var artistBtn = document.createElement('<a>');
-        artistBtn.document.addclass('button')
-        artistBtn.textContent = search;
-        artistBtn.classList.add('btn');
-        btnGroup.append(artistBtn);
-    })
+  var searchInputVal = this.textContent;
+
+  getParams(searchInputVal);
 }
 
-function getParams(query){
-    console.log(query)
-
-    if (recentSearchArr.length <=4) {
-        recentSearchArr.push(query);
-        localStorage.setItem(i, JSONstringify(searchHistoryArr));
-    }
-    getArtistInfo(query)
-}
 //^^^Eric's modal function^^^
 
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+  console.log("works");
 
-function handleSearchFormSubmit (event) {
-	event.preventDefault();
-console.log('works');
+  var searchInputVal = document.querySelector(".input").value;
 
-var searchInputVal = document.querySelector('.input').value;
+  if (!searchInputVal) {
+    modal.style.display = "block";
+    //window.alert('Please enter an Artist to search for.')
+  }
 
-    if (!searchInputVal) {
-        modal.style.display = 'block';
-        //window.alert('Please enter an Artist to search for.')
-    }
-
-    getParams()
+  getParams();
 }
 
-function getParams (search){
-    var search = input.value;
-    console.log(search);
+function getParams(search) {
+  var search = input.value;
+  console.log(search);
 
-    getArtistInfo(search);
-    getArtistEvent(search);
+  if (recentSearchArr.length <= 10) {
+    recentSearchArr.push(search);
+    localStorage.setItem('previousSearches', JSON.stringify(recentSearchArr));
+  }
+
+  getArtistInfo(search);
+  getArtistEvent(search);
 }
-
 
 function getArtistInfo(search) {
-    var apiArtist = "https://theaudiodb.com/api/v1/json/1/search.php?s=" + (search);
+  var apiArtist = "https://theaudiodb.com/api/v1/json/1/search.php?s=" + search;
 
-    fetch(apiArtist)
+  fetch(apiArtist)
     .then(function (response) {
-    console.log(response.ok);
-    if (!response.ok) {
-        window.alert('Unable to connect');
+      console.log(response.ok);
+      if (!response.ok) {
+        window.alert("Unable to connect");
         throw response.json();
-}
+      }
 
-return response.json()
-})
-    .then(function (artistResults){
-        printArtistResults(artistResults);
+      return response.json();
     })
-
+    .then(function (artistResults) {
+      printArtistResults(artistResults);
+    });
 }
 
 function getArtistEvent(search) {
-    var apiEvents = "https://app.ticketmaster.com/discovery/v2/attractions.json?" + "keyword=" + (search) + "&apikey=" + "5pHWUAmDnwF3nJzxzlERJBtBG2o4Rl4q";
+  var apiEvents =
+    "https://app.ticketmaster.com/discovery/v2/attractions.json?" +
+    "keyword=" +
+    search +
+    "&apikey=" +
+    "5pHWUAmDnwF3nJzxzlERJBtBG2o4Rl4q";
 
-    fetch(apiEvents)
+  fetch(apiEvents)
     .then(function (response) {
-    console.log(response.ok);
-    if (!response.ok) {
-        window.alert('Unable to connect');
+      console.log(response.ok);
+      if (!response.ok) {
+        window.alert("Unable to connect");
         throw response.json();
-}
+      }
 
-return response.json()
-})
-    .then(function (eventResults){
-        // printEventResults(eventResults);
-        var resultArr = eventResults._embedded.attractions
-        console.log(resultArr)
-        //this is best bet if you want some sort of filtering happening
-        //to pass through to printEventResults fn
-        var filteredArr = resultArr.filter(function(index) {
-          return index.name.toLowerCase().includes(search.toLowerCase())
-        } )
-        console.log(filteredArr)
+      return response.json();
     })
-
+    .then(function (eventResults) {
+      // printEventResults(eventResults);
+      var resultArr = eventResults._embedded.attractions;
+      console.log(resultArr);
+      //this is best bet if you want some sort of filtering happening
+      //to pass through to printEventResults fn
+      var filteredArr = resultArr.filter(function (index) {
+        return index.name.toLowerCase().includes(search.toLowerCase());
+      });
+      console.log(filteredArr);
+    });
 }
 
-function printArtistResults (artistResults){
-    console.log(artistResults);
-    var artistName = document.querySelector('.title');
-    artistResults.artists ? artistName.innerText = artistResults.artists[0].strArtist : artistName.innerText = "No";
-    var artistPicture = document.querySelector('.artist-pic');
-    artistPicture.src = artistResults.artists[0].strArtistThumb;
-//inner html for image
+
+function printArtistResults(artistResults) {
+  console.log(artistResults);
+
+  
+  var artistName = document.querySelector(".title");
+  artistResults.artists
+    ? (artistName.innerText = artistResults.artists[0].strArtist)
+    : (artistName.innerText = "No");
+  var artistPicture = document.querySelector(".artist-pic");
+  artistPicture.src = artistResults.artists[0].strArtistThumb;
+  var artistCountry = document.querySelector(".country");
+  artistCountry.innerText = artistResults.artists[0].strCountry;
+  //inner html for image
 }
 
-function printEventResults (eventResults){
-    console.log(eventResults);
-	}	
+function printEventResults(eventResults) {
+  console.log(eventResults); 
+}
 
 
-	button.addEventListener('click', handleSearchFormSubmit);
-
+button.addEventListener("click", handleSearchFormSubmit); 
